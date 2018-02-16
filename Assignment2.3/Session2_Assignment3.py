636f01da2b4dc36456c138816c452a8fef20025b
sasmita-sabat <35617622+sasmita-sabat@users.noreply.github.com>
2018-02-16 21:53:18 +0530

    Assignment3 for session2

---
+++ Assignment2.3/Session2_Assignment3.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#Input a string and reverse the string
+Orig_str=input("Enter the string which needs to be reversed ")
+Rev_str=Orig_str[::-1]
+print("Reverse of the string is " + Rev_str)
+
